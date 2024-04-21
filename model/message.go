3c4883793f6be62ee200939f2a1b804001bf18fe
aprilian tantra <tantraluhur@gmail.com>
2024-04-21 17:03:11 +0700

    feat: init chat

--- model/message.go
+++ model/message.go
@@ -2,6 +2,7 @@ package models
 
 type Message struct {
 	ID            string `json:"id" gorm:"primaryKey"`
+	Message       string `json:"message"`
 	ChatSessionId string `json:"chat_session"`
 	UserID        int64  `json:"user"`
 
